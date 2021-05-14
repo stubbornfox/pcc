12eded1a8ac7dc621bd2e3aed8a80226928cbfdd
Niclas van Eyk <niclas.eyk@gmail.com>
2021-05-14 14:58:40 +0200

    segmentation refactorings and global clustering
    Now the segmentation and interpretation by googlenet
    is done in one go. We can keep all data in memory, so
    there is no need to write the segments to disk now.
    
    "Checkpoint" files are created, so that we do not always
    have to redo the segmentation and are able to quickly
    load the dataset into memory (takes around a second
    or so).

--- segmentize.py
+++ segmentize.py
@@ -1,13 +1,16 @@
-from skimage.segmentation import felzenszwalb, slic, quickshift, watershed
-from skimage.segmentation import mark_boundaries
-from skimage.util import img_as_float
+import os.path
+import pickle
+
+import torch
+from skimage.segmentation import slic
 from PIL import Image
 import numpy as np
-import matplotlib.pyplot as plt
-import os
-import glob
+from glob import glob
+from torchvision.transforms import transforms
+from tqdm import tqdm
+from Segment import Segment
 
-def _extract_patch(image, mask, average_image_value=117):
+def _extract_segments(image, mask, average_image_value=117):
     mask_expanded = np.expand_dims(mask, -1)
     patch = (mask_expanded * image + (
       1 - mask_expanded) * float(average_image_value) / 255)
@@ -39,38 +42,82 @@ def _return_superpixels(im2arr):
                 if unique:
                     param_masks.append(mask)
         unique_masks.extend(param_masks)
-        superpixels, patches = [], []
+        superpixels, segments = [], []
         while unique_masks:
-            superpixel, patch = _extract_patch(im2arr, unique_masks.pop())
+            superpixel, segment = _extract_segments(im2arr, unique_masks.pop())
             superpixels.append(superpixel)
-            patches.append(patch)
+            segments.append(segment)
 
-        return superpixels, patches
-        # print(segments_slic)
-        # plt.imshow(mark_boundaries(img, segments_slic))
-        # plt.show()
-# dataset, image_numbers, patches = [], [], []
-train_patches_save_path = os.path.join('data/CUB_200_2011', 'dataset/train_patches/')
-path = 'data/CUB_200_2011/dataset/train_crop/*/*'
-index  = 0
-shape = (224, 224)
-for k in glob.glob(path):
-    img = Image.open(k)
-    im2arr = np.array(img.resize(shape, Image.BILINEAR))
-    # Normalize pixel values to between 0 and 1.
-    im2arr = np.float32(im2arr) / 255.0
-    file_name = k.split('/')[-2]
-    print(k)
-    if not os.path.isdir(train_patches_save_path + file_name):
-        os.makedirs(os.path.join(train_patches_save_path, file_name))
-        index = 0
-    image_superpixels, image_patches = _return_superpixels(im2arr)
-    for superpixel, patch in zip(image_superpixels, image_patches):
-        index += 1
-        # print(superpixel)
-        im = superpixel
-            #Image.fromarray((superpixel * 255).astype(np.uint8))
-        im.save(os.path.join(os.path.join(train_patches_save_path, file_name), str(index)+'.jpg'))
-# sdataset, simage_numbers, spatches = \
-#     np.array(dataset), np.array(image_numbers), np.array(patches)
-# print(sdataset.shape)
+        return superpixels, segments
+
+def _images_in_folder(folder_glob):
+    """Iteration logic to load up each image from the birds"""
+    outer_progress = tqdm(glob(folder_glob), position=0, leave=False)
+
+    for folder_path in outer_progress:
+        # Folder names are of the form "001.Black_footed_Albatross"
+        folder_name = folder_path.split("/")[-1]
+        bird_id, bird_name = folder_name.split('.')
+        images = glob(folder_path + '/*.jpg')
+
+        for image_file_path in tqdm(images, desc=bird_name, position=1, leave=False):
+            outer_progress.refresh()
+            # File names are of the form "Black_Footed_Albatross_0001_796111.jpg"
+            file_name = image_file_path.split('/')[-1]
+            suffix = file_name.upper().replace(bird_name.upper() + '_', '')
+            image_id = int(suffix.split('_')[0])
+            yield int(bird_id), image_id, Image.open(image_file_path)
+
+def segment_source_images(
+    source_image_directory='data/CUB_200_2011/dataset/train_crop/*',
+    force_recreation=False,
+    checkpoint_file='data/checkpoints/segments.obj',
+) -> list[Segment]:
+    """
+    :param source_image_directory: Optional path to the cropped image folders
+    :param force_recreation: recreate the checkpoint file instead of reusing it
+    :param checkpoint_file: path to the checkpoint file
+    :return: segments with attached metadata e.g. feature vector obtained from googlenet
+    """
+    if not force_recreation and os.path.isfile(checkpoint_file):
+        print(f'Reading segments from cache ({checkpoint_file})...')
+        with open(checkpoint_file, 'rb') as checkpoint:
+            return pickle.load(checkpoint)
+    else:
+        print('Creating image segments...')
+
+    segments = []
+    model = torch.hub.load('pytorch/vision:v0.9.0', 'googlenet', pretrained=True)
+    model.eval()
+    preprocessing_pipeline = transforms.Compose([
+        transforms.ToTensor(),
+        # Values obtained from Prototree paper
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    for bird_id, image_id, image in _images_in_folder(source_image_directory):
+        im2arr = np.array(image.resize((224, 224), Image.BILINEAR))
+        # Normalize pixel values to between 0 and 1.
+        im2arr = np.float32(im2arr) / 255.0
+        superpixels, _ = _return_superpixels(im2arr)
+        for i, segment in enumerate(superpixels):
+            segment_data = np.array(segment)
+            segment_tensor = preprocessing_pipeline(segment)
+            with torch.no_grad():
+                mini_batch = segment_tensor.unsqueeze(0)
+                features = model(mini_batch)
+
+            segments.append(Segment(
+                class_id=bird_id,
+                image_id=image_id,
+                segment_id=i,
+                raw=segment_data,
+                features=features
+            ))
+
+    print("Writing checkpoint file...")
+    os.makedirs(os.path.dirname(checkpoint_file), exist_ok=True)
+    with open(checkpoint_file, 'wb') as checkpoint:
+        pickle.dump(segments, checkpoint)
+
+    return segments
