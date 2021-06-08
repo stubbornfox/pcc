620462ef9e76ce0ad07d6d1684bd25ec628b5e6c
Niclas van Eyk <niclas.eyk@gmail.com>
2021-06-08 18:25:11 +0200

    bumped the config up to all classes and 1000 clusters

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ from utils.dataset import Dataset
 from os.path import realpath, dirname, join
 
 configuration = Configuration(
-    num_classes=90,
-    num_clusters=400,
+    num_classes=200,
+    num_clusters=1000,
     cluster_accuracy_threshold=50,
 )
 dataset = Dataset(
@@ -13,8 +13,14 @@ dataset = Dataset(
     configuration=configuration,
 )
 
+# First 100
 # | Clusters | Train | Test |
 # |==========|=======|======|
 # |      200 |    73 |   50 |
 # |      400 |    82 |   58 |
 # |     1000 |    77 |   52 |
+
+# All Birds
+# |      600 |    79 |   54 |
+# |     1000 |       |      |
+# |          |       |      |
