2eb8e76fb03c69b35be38f0dfb49be93658348a0
Niclas van Eyk <niclas.eyk@gmail.com>
2021-06-13 12:42:48 +0200

    Always print the configuration
    Maybe this helps a bit

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict
+
 from utils.configuration import Configuration
 from utils.dataset import Dataset
 
@@ -8,7 +10,15 @@ configuration = Configuration(
     num_clusters=60,
     cluster_accuracy_threshold=25,
 )
-# configuration.use_cropped_images = True
+configuration.use_cropped_images = True
+
+readable = '\n'.join([
+    f'{key}: {value}'
+    for key, value
+    in asdict(configuration).items()
+])
+print(f'Configuration:\n{readable}')
+
 
 dataset = Dataset(
     base_path=join(realpath(dirname(__file__)), 'data', 'CUB_200_2011'),
